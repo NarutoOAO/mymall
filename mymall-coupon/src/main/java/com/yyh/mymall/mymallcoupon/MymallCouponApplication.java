e34bb7fe10ac4a8de4b0f47ef83219bcb033c386
NarutoOAO <2371848072@qq.com>
2023-11-24 11:06:39 +0800

    add codes

---
+++ mymall-coupon/src/main/java/com/yyh/mymall/mymallcoupon/MymallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.yyh.mymall.mymallcoupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MymallCouponApplication.class, args);
+    }
+
+}
---
+++ mymall-ware/src/test/java/com/yyh/mymall/mymallware/MymallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyh.mymall.mymallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
